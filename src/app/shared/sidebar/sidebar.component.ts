9a8a3dd6015b33258c19e9bbee43a5fc0b7de00e
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2022-02-27 22:11:55 -0500

    Add files via upload

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { GifsService } from 'src/app/gifs/services/gifs.service';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styles: []
+})
+export class SidebarComponent {
+
+  get historial() {
+    return this.gifsService.historial;
+  }
+
+  buscar(termino: any) {
+    this.gifsService.buscarGifs(termino);
+
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+
+}
