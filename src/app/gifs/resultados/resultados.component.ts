b4d3aef010397d85bc38ca8147957ff862f2f8c0
Sammy Gigi Cantoral Montejo <44757651+sgcm14@users.noreply.github.com>
2022-02-27 22:09:37 -0500

    Add files via upload

---
+++ src/app/gifs/resultados/resultados.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
+
+@Component({
+  selector: 'app-resultados',
+  templateUrl: './resultados.component.html',
+  styles: [
+  ]
+})
+export class ResultadosComponent implements OnInit {
+
+  get resultados() {
+    return this.gifsService.resultados;
+  }
+
+  constructor(private gifsService: GifsService) { }
+
+  ngOnInit(): void {
+  }
+
+}
